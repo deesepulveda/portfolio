205883780598cec3820129cc33e74aea50eff9b5
dee chao <chaoserady@gmail.com>
2022-10-05 23:23:22 -0500

    fixed contact form size and update nav

--- app.js/script.js
+++ app.js/script.js
@@ -6,6 +6,9 @@ const topLine = document.querySelector(".top-line");
 const bottomLine = document.querySelector(".bottom-line");
 const navLinks = document.querySelectorAll(".hero-nav-links");
 
+// Media Queries
+const mediaQueryDesktop = window.matchMedia("(min-width: 1280px)");
+
 // Show Nav Menu Function
 const showNavMenu = () => {
   nav.classList.toggle("showNav");
@@ -21,6 +24,15 @@ burger.addEventListener("click", () => {
 // Close Nav Menu after Clicking Links
 navLinks.forEach((l) => {
   l.addEventListener("click", () => {
-    showNavMenu();
+    if (nav.classList.contains("showNav")) showNavMenu();
   });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 5 && mediaQueryDesktop.matches === true) {
+    nav.classList.add("navBackground");
+    console.log("It is working");
+  } else {
+    nav.classList.remove("navBackground");
+  }
 });
